63fa61d3a28e1c8da93958dbdd689eb0cd325c6b
csantagata <99364934+mycsantagata@users.noreply.github.com>
2024-06-09 21:05:43 +0200

    Update BalanceBoard.tsx

--- src/components/BalanceBoard.tsx
+++ src/components/BalanceBoard.tsx
@@ -2,32 +2,38 @@ import {Card} from 'react-bootstrap'
 import { useAccount, useDisconnect , useBalance} from 'wagmi';
 import {Button} from 'react-bootstrap';
 import {formatEther} from 'viem';
-import { useEffect } from 'react';
 
 const BalanceBoard: React.FC = () => {
   const {disconnect} = useDisconnect();
-  const {address} = useAccount();
+  const {address, chain} = useAccount();
 
   const balance = useBalance({
     address: address
   });
 
-  useEffect(() => {
-  }, [address]);
-
   const balanceEther = formatEther(balance?.data?.value ?? BigInt(0))
   const formattedBalance = (+balanceEther).toFixed(4);
 
   return (
-    <Card className='border-3 border-dark shadows mb-5 enlarge'>
-  <Card.Body className="d-flex justify-content-between">
-    <div className='balanceBoard-title'><h2>Travel Dapp</h2></div>
-    <div className="d-flex">
-      <h3 style={{marginRight: '40px'}}>Balance: <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/16px-Ethereum_logo_2014.svg.png"/> {formattedBalance}</h3>
-      <Button variant='primary' onClick={() => disconnect()}>Disconnect</Button>
-    </div>
-  </Card.Body>
-</Card>
+<Card className='border-3 border-dark shadows mb-5 enlarge'>
+      <Card.Body className="d-flex justify-content-between">
+        <div className='balanceBoard-title'>
+          <h2>Travel Dapp</h2>
+        </div>
+        <div className="d-flex align-items-center">
+          <h5 style={{ marginRight: '20px' }}>
+            Address: {address?.slice(0, 6)}...{address?.slice(-4)}
+          </h5>
+          <h5 style={{ marginRight: '20px' }}>
+            Network: {chain?.name ?? 'Unknown Network'}
+          </h5>
+          <h5 style={{ marginRight: '20px' }}>
+            Balance: {chain?.nativeCurrency.symbol} {formattedBalance}
+          </h5>
+          <Button variant='primary' onClick={() => disconnect()}>Disconnect</Button>
+        </div>
+      </Card.Body>
+    </Card>
   )
 }
 
