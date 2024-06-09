b0d0e4a3348ff27e7123a6a28997c61b71bbd142
csantagata <99364934+mycsantagata@users.noreply.github.com>
2024-06-09 21:12:02 +0200

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,15 +1,17 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { walletConnect} from 'wagmi/connectors'
+import { mainnet, sepolia, polygon } from 'wagmi/chains'
+import { walletConnect } from 'wagmi/connectors'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia, polygon],
   connectors: [
-    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID })
+    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
   ],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [polygon.id]: http(),
+    
   },
 })
 
